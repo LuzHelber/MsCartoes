6cb6e701bb1ec2d044df369562931020da93a764
Luz Helber <helbersap@gmail.com>
2023-05-03 11:30:43 -0300

    implementação de anotações e configuração da fila

--- mscartoes/src/main/java/br/com/helber/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/br/com/helber/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -1,12 +1,15 @@
 package br.com.helber.mscartoes.infra.mqueue;
 
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class EmissaoCartaoSubscriber {
 
-
-    public void receberSolicitacaoEmissao(String payload) {
+    @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
+    public void receberSolicitacaoEmissao(@Payload String payload) {
         System.out.println(payload);
     }
 }
